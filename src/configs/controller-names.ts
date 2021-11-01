e20d12f0e7bd4a8a31adf35105d7b2d3b7710c7b
Vardan Hambardzumyan <vardanhambardzumyan.bangits@gmail.com>
2021-11-01 12:44:58 +0400

    all get calls were refactored

---
+++ src/configs/controller-names.ts
@@ -0,0 +1,10 @@
+export const controllerNames = {
+  Countries: '/Countries',
+  Currencies: '/Currencies',
+  Languages: '/Languages',
+  PhoneCode: '/PhoneCode',
+  DocumentType: '/DocumentType',
+  Genders: '/Genders',
+  Region: '/Region',
+  CityVillage: '/CityVillage'
+};
--- src/domain/data/ResourceManagerRepository.ts
+++ src/domain/data/ResourceManagerRepository.ts
@@ -1,3 +1,4 @@
+import { controllerNames } from '@/configs';
 import { cachedFn } from '@/helpers';
 import { ICacheService, IHttpService } from '@/services';
 import { inject, injectable } from 'inversify';
@@ -27,72 +28,72 @@ export class ResourceManagerRepository implements IResourceManagerRepository {
 
   getCountries = cachedFn('CachedCountries', async (getCountriesRequestModel: FilterRequestModel) => {
     const countries = await this.httpService.get<GetCountriesResponseModel, FilterRequestModel>({
-      url: '/Countries',
+      url: controllerNames.Countries,
       query: getCountriesRequestModel
     });
 
     return countries;
   }).bind(this);
 
-  getCurrency = async (getCurrencyRequestModel: FilterRequestModel) => {
+  getCurrency = cachedFn('CachedCurrencies', async (getCurrencyRequestModel: FilterRequestModel) => {
     const currency = await this.httpService.get<GetCurrencyResponseModel, FilterRequestModel>({
-      url: '/Currencies',
+      url: controllerNames.Currencies,
       query: getCurrencyRequestModel
     });
 
     return currency;
-  };
+  }).bind(this);
 
-  getLanguage = async (getLanguageRequestModel: FilterRequestModel) => {
+  getLanguage = cachedFn('CachedLanguages', async (getLanguageRequestModel: FilterRequestModel) => {
     const language = await this.httpService.get<GetLanguageResponseModel, FilterRequestModel>({
-      url: '/Languages',
+      url: controllerNames.Languages,
       query: getLanguageRequestModel
     });
 
     return language;
-  };
+  }).bind(this);
 
-  getPhoneCode = async (getPhoneCodeRequestModel: FilterRequestModel) => {
+  getPhoneCode = cachedFn('CachedPhoneCodes', async (getPhoneCodeRequestModel: FilterRequestModel) => {
     const phoneCode = await this.httpService.get<GetPhoneCodeResponseModel, FilterRequestModel>({
-      url: '/PhoneCode',
+      url: controllerNames.PhoneCode,
       query: getPhoneCodeRequestModel
     });
 
     return phoneCode;
-  };
+  }).bind(this);
 
-  getDocumentType = async (getDocumentTypeRequestModel: FilterRequestModel) => {
+  getDocumentType = cachedFn('CachedDocumentTypes', async (getDocumentTypeRequestModel: FilterRequestModel) => {
     const documentType = await this.httpService.get<GetDocumentTypeResponseModel, FilterRequestModel>({
-      url: '/DocumentType',
+      url: controllerNames.DocumentType,
       query: getDocumentTypeRequestModel
     });
 
     return documentType;
-  };
+  }).bind(this);
 
-  getGender = async () => {
+  getGender = cachedFn('CachedGenders', async () => {
     const genderType = await this.httpService.get<GetGenderResponseModel, FilterRequestModel>({
-      url: '/Gender' // TO DO, need to correct after backend will add api
+      url: controllerNames.Genders
     });
 
     return genderType;
-  };
+  }).bind(this);
 
-  getRegion = async (getRegionRequestModel: RegionFilterRequestModel) => {
+  getRegion = cachedFn('CachedRegions', async (getRegionRequestModel: RegionFilterRequestModel) => {
     const region = await this.httpService.get<GetRegionResponseModel, RegionFilterRequestModel>({
-      url: '/Region', // TO DO, need to correct after backend will add api
+      url: controllerNames.Region,
       query: getRegionRequestModel
     });
 
     return region;
-  };
+  }).bind(this);
 
-  getCityVillage = async (getCityVillageRequestModel: CityVillageFilterRequestModel) => {
+  getCityVillage = cachedFn('CachedCityVillages', async (getCityVillageRequestModel: CityVillageFilterRequestModel) => {
     const cityVillage = await this.httpService.get<GetCityVillageResponseModel, CityVillageFilterRequestModel>({
-      url: '/CityVillage', // TO DO, need to correct after backend will add api
+      url: controllerNames.CityVillage,
       query: getCityVillageRequestModel
     });
 
     return cityVillage;
-  };
+  }).bind(this);
 }
