cb6435da0b0f897246b06e211b5d54a1225f0ac1
Armen Mesropyan <amesropyan@bangits.com>
2021-11-01 23:08:03 +0400

    .

--- src/services/CacheService.ts
+++ src/services/CacheService.ts
@@ -4,7 +4,6 @@ export interface ICacheService {
   get<T>(key: string): T;
   set<T>(key: string, value: T): void;
 }
-
 @injectable()
 export class CacheService implements ICacheService {
   private readonly cachedData = {};
