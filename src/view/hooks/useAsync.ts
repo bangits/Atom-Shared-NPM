7195aaa33bcdd5a463453552b0d1f691b32de779
Armen Mesropyan <amesropyan@bangits.com>
2021-11-18 14:38:02 +0400

    Added dependencies argument in useAsync hook

--- src/view/hooks/useAsync.ts
+++ src/view/hooks/useAsync.ts
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 
 export const useAsync = <T extends (...args: any[]) => Promise<any>, K = undefined>(
   fn: T,
+  dependencies: any[] = [],
   defaultValue: K = undefined
 ): AsyncReturnType<T> | K => {
   const [returnValue, setReturnValue] = useState<AsyncReturnType<T> | K>(defaultValue);
 
   useEffect(() => {
     fn().then((value) => setReturnValue(value));
-  }, []);
+  }, dependencies);
 
   return returnValue;
 };
