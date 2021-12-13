50795433709d43f0098c107946a4da29bb35a1a3
Ruben <rubenabelyan.bangits@gmail.com>
2021-12-13 12:19:06 +0400

    Added Input Select

--- src/view/components/shared/index.ts
+++ src/view/components/shared/index.ts
@@ -1,6 +1,8 @@
 export * from './ChangedSelect';
+export * from './CustomInput';
 export * from './CustomSelect';
 export * from './FileUploader';
 export * from './LoadingServiceComponent';
 export * from './Stepper';
 export * from './TelephoneInput';
+
