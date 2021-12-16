48bdc06dd2fdf48a52bc0f2fed2e73510289d8ca
Armen Mesropyan <amesropyan@bangits.com>
2021-12-16 20:13:17 +0400

    Updated the version

--- src/view/hooks/useLocation.ts
+++ src/view/hooks/useLocation.ts
@@ -4,7 +4,7 @@ export const useLocation = () => {
   const [location, setLocation] = useState(window.location);
 
   useEffect(() => {
-    // @ts-ignore Ignoring typescript becuase in window events they're not single-spa:before-routing-event
+    // @ts-expect-error Ignoring typescript becuase in window events they're not single-spa:before-routing-event
     window.addEventListener('single-spa:before-routing-event', (evt: { detail: { newUrl: string } }) =>
       setLocation({ ...window.location })
     );
