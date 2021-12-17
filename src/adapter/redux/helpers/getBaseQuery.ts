e19e6b4e1511f1689843a0784744d13154e03ea4
Ruben <rubenabelyan.bangits@gmail.com>
2021-12-17 16:52:00 +0400

    fixed getBaseQuery

--- src/adapter/redux/helpers/getBaseQuery.ts
+++ src/adapter/redux/helpers/getBaseQuery.ts
@@ -1,11 +1,9 @@
 import { DiContainer } from '@/di';
 import { BaseQueryFn } from '@reduxjs/toolkit/query';
 import { immerable } from 'immer';
-
 interface CreateBaseQueryArgument {
   useCaseName: string;
 }
-
 interface CreateBaseQueryReturnType<T extends Record<string, (...args: any[]) => void>>
   extends BaseQueryFn<
     {
@@ -15,24 +13,18 @@ interface CreateBaseQueryReturnType<T extends Record<string, (...args: any[]) =>
     unknown,
     { message: string }
   > {}
-
 export const getBaseQuery =
   (containerInstance: DiContainer) =>
   <T extends {}>({ useCaseName }: CreateBaseQueryArgument): CreateBaseQueryReturnType<T> =>
   async ({ methodName, methodArguments }) => {
     try {
       const useCase = containerInstance.diContainer.get(useCaseName);
-
       const method = useCase[methodName] as (...args: any[]) => Promise<unknown>;
-
       const data = await method(...(methodArguments as any[]));
-
       if (typeof data === 'object') data[immerable] = true;
-
       return { data };
     } catch (error) {
       if (error.response?.data?.Status) return Promise.reject(error.response.data.Status);
-
       return Promise.reject(error);
     }
   };
