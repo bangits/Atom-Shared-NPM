c22f484cd63b2c6d26657703872fb0f3834080be
MiqayelGalstyanBangits <m.galstyan@bangits.com>
2023-05-29 12:08:43 +0400

    added new translations

--- src/common/translations/index.ts
+++ src/common/translations/index.ts
@@ -1861,5 +1861,9 @@ export default {
   eiigibleGameEmptyText: 'There are no added Eligible Games in current Bonus.',
   supportedLangagues: 'Supported Countries',
   isBonusBet: 'Is Bonus Bet',
-  freeSpinsValue:'Free Spins Value'
+  freeSpinsValue:'Free Spins Value',
+  freeSpinsValueFrom:'Free Spins Value From',
+  freeSpinsValueTo:'Free Spins Value To',
+  freeSpinsCountFrom: 'Free Spins Count From',
+  freeSpinsCountTo: 'Free Spins Count To',
 };
