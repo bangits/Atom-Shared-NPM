53036f3130d7b03bf4a5542a007f8ecd15ee0fad
Gor Sargsyan <g.sargsyan@bangits.com>
2023-10-10 12:26:43 +0400

    fix: panda config issue

--- panda.config.js
+++ panda.config.js
@@ -1,7 +1,4 @@
 import { defineConfig } from '@pandacss/dev';
 import { pandaConfig } from '@atom/design-system/configs';
 
-export default defineConfig({
-    ...pandaConfig,
-    include: [...pandaConfig.include, './src/lab/**/*.{ts,tsx}']
-});
+export default defineConfig(pandaConfig);
